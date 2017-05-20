3d08e3384833158f303416a02031769223b2637b
kiven <kiven.mr@gmail.com>
2017-05-20 11:56:03 +0800

    Enhance

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,26 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value: value,
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 
 fn main() {
     println!("Guess the number!");
@@ -22,7 +42,9 @@ fn main() {
             Err(_)  => continue,
         };
 
-        println!("Your guessed: {}", guess);
+        let g_s = Guess::new(guess);
+
+        println!("Your guessed: {}", g_s.value);
 
         match guess.cmp(&secret_num) {
             Ordering::Less    => println!("Too small!"),
